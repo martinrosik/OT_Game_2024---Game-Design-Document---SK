e854a9001aed67902d540eb3043e2f3456d324af
Martin <133987180+martinthetech@users.noreply.github.com>
2025-01-18 12:48:00 +0100

    increase items lifetime

--- assets/items/torch.py
+++ assets/items/torch.py
@@ -6,7 +6,7 @@ class Torch(pygame.sprite.Sprite):
         self.image = surface
         self.rect = self.image.get_rect(topleft=position)
         self.spawn_time = pygame.time.get_ticks()
-        self.lifetime = 45000
+        self.lifetime = 60000
 
     def update(self, delta):
         if pygame.time.get_ticks() - self.spawn_time >= self.lifetime:
